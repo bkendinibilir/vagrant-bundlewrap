938f72b9b128e3b4bf2415c5c2e2cafe5b15be91
Torsten Rehn <torsten@rehn.email>
2015-01-08 11:30:27 +0100

    whitespace
    no code changes, just indent using two spaces and manually resolve mixed tabs and spaces

--- lib/vagrant-bundlewrap/bwmanage.rb
+++ lib/vagrant-bundlewrap/bwmanage.rb
@@ -1,47 +1,46 @@
 module VagrantPlugins
-	module BundleWrap
-		class BwManage
+  module BundleWrap
+    class BwManage
 
-			def initialize(repo_path)
-				@repo_path = repo_path
-			end
+      def initialize(repo_path)
+        @repo_path = repo_path
+      end
 
-			def bw_cli(params, ret_stdout=true)
-				params = params.gsub(/[^a-zA-Z0-9\-\_\.\s]/,'')
-				old_path = Dir.pwd
-				Dir.chdir(@repo_path)
-				if ret_stdout
-					result = `bw #{params}`
-				else
-					result = system("bw #{params}")
-				end
-				Dir.chdir(old_path)
-				return result
-			end
+      def bw_cli(params, ret_stdout=true)
+        params = params.gsub(/[^a-zA-Z0-9\-\_\.\s]/,'')
+        old_path = Dir.pwd
+        Dir.chdir(@repo_path)
+        if ret_stdout
+          result = `bw #{params}`
+        else
+          result = system("bw #{params}")
+        end
+        Dir.chdir(old_path)
+        return result
+      end
 
-			def nodes()
-				nodes = bw_cli("nodes")
-				return nodes.split("\n")
-			end
+      def nodes()
+        nodes = bw_cli("nodes")
+        return nodes.split("\n")
+      end
 
-			def node_hosts()
-				nodes = bw_cli("nodes --hostname")
-				return nodes.split("\n")
-			end
+      def node_hosts()
+        nodes = bw_cli("nodes --hostname")
+        return nodes.split("\n")
+      end
 
-			def apply(node, debug=false, interactive=false)
-				node = node.gsub(/[^a-zA-Z0-9\-\_\.]/,'')
-				cmd = ""
-				if debug
-					cmd = cmd + "--debug "
-				end
-				cmd = cmd + "apply "
-				if interactive
-					cmd = cmd + "--interactive "
-				end
-				return bw_cli(cmd + node, ret_stdout=false)
-			end
-
-		end
-	end
+      def apply(node, debug=false, interactive=false)
+        node = node.gsub(/[^a-zA-Z0-9\-\_\.]/,'')
+        cmd = ""
+        if debug
+          cmd = cmd + "--debug "
+        end
+        cmd = cmd + "apply "
+        if interactive
+          cmd = cmd + "--interactive "
+        end
+        return bw_cli(cmd + node, ret_stdout=false)
+      end
+    end
+  end
 end
--- lib/vagrant-bundlewrap/config.rb
+++ lib/vagrant-bundlewrap/config.rb
@@ -1,49 +1,48 @@
 module VagrantPlugins
-	module BundleWrap
-    	class Config < Vagrant.plugin("2", :config)
-            attr_accessor :repo_path
-    		attr_accessor :node_name
-    		attr_accessor :node_host
-    		attr_accessor :debug
-            attr_accessor :interactive
+  module BundleWrap
+    class Config < Vagrant.plugin("2", :config)
+      attr_accessor :repo_path
+      attr_accessor :node_name
+      attr_accessor :node_host
+      attr_accessor :debug
+      attr_accessor :interactive
 
+      def initialize
+        @repo_path = UNSET_VALUE
+        @node_name = UNSET_VALUE
+        @node_host = UNSET_VALUE
+        @interactive = UNSET_VALUE
+        @debug = UNSET_VALUE
+      end
 
-    		def initialize
-                @repo_path = UNSET_VALUE
-    			@node_name = UNSET_VALUE
-    			@node_host = UNSET_VALUE
-                @interactive = UNSET_VALUE
-                @debug = UNSET_VALUE
-    		end
+      def finalize!
+        @repo_path = "bundlewrap/" if @repo_path == UNSET_VALUE
+        @node_name = nil if @node_name == UNSET_VALUE
+        @node_host = nil if @node_host == UNSET_VALUE
+        @debug = false if @debug == UNSET_VALUE
+        @interactive = false if @interactive == UNSET_VALUE
+      end
 
-    		def finalize!
-    			@repo_path = "bundlewrap/" if @repo_path == UNSET_VALUE
-                @node_name = nil if @node_name == UNSET_VALUE
-    			@node_host = nil if @node_host == UNSET_VALUE
-			@debug = false if @debug == UNSET_VALUE
-                @interactive = false if @interactive == UNSET_VALUE
-    		end
+      def validate(machine)
+        errors = _detected_errors
 
-            def validate(machine)
-                errors = _detected_errors
+        unless File.directory?(repo_path) and File.exist?("#{repo_path}/nodes.py") and File.exist?("#{repo_path}/groups.py")
+            errors << "BundleWrap repository is missing in repo_path='#{repo_path}'.\n" +
+                "  Create a new one with 'bw repo create'."
+        end
 
-                unless File.directory?(repo_path) and File.exist?("#{repo_path}/nodes.py") and File.exist?("#{repo_path}/groups.py")
-                    errors << "BundleWrap repository is missing in repo_path='#{repo_path}'.\n" +
-                        "  Create a new one with 'bw repo create'."
-                end
+        unless @node_name
+            errors << "BundleWrap config 'node_name' is not set. Use the node name from your BundleWrap nodes.py."
+        end
 
-                unless @node_name
-                    errors << "BundleWrap config 'node_name' is not set. Use the node name from your BundleWrap nodes.py."
-                end
+        unless @node_host
+            errors << "BundleWrap config 'node_host' is not set. Use the node host from your BundleWrap nodes.py.\n" +
+                "  This does not have to be a valid host name (e.g. 'node1.vm'), but it should be a unique one,\n" +
+                "  because it will be written in your ssh config file."
+        end
 
-                unless @node_host
-                    errors << "BundleWrap config 'node_host' is not set. Use the node host from your BundleWrap nodes.py.\n" +
-                        "  This does not have to be a valid host name (e.g. 'node1.vm'), but it should be a unique one,\n" +
-                        "  because it will be written in your ssh config file."
-                end
-
-                { "vagrant-bundlewrap" => errors }
-              end
-    	end
-	end
+        { "vagrant-bundlewrap" => errors }
+      end
+    end
+  end
 end
